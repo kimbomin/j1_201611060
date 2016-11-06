7bc747d7879a909df51b7e1900c1e5652a9a3ea7
kimbomin <tbmom13@naver.com>
2016-11-06 22:32:40 +0900

    grade

---
+++ Grade.java
@@ -0,0 +1,24 @@
+﻿public class Grade {
+   String score;
+   String grade(double marks) {  
+      if (90<=marks && marks<=100) {  
+      score="A";
+      } 
+      else if (80<=marks && marks<90) {  
+      score="B"; 
+      } 
+      else if (70<=marks && marks<80) {  
+      score="C";  
+      } 
+      else if (60<=marks && marks<70) {  
+      score="D";  
+      } 
+      else {  
+      score="F or input error";
+      }      
+      return score;
+   }
+public static void main(String[] args) {
+   System.out.printf("Your grade is %s",new Grade().grade(80)); 
+   }
+}
