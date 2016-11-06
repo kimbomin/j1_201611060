67df7f178d67be8bdc71f26960c1f8da97766977
kimbomin <tbmom13@naver.com>
2016-11-06 22:32:48 +0900

    bmi

---
+++ BMI.java
@@ -0,0 +1,31 @@
+﻿public class BMI { 
+   public String bmi(float height, float weight) { 
+   float bmi = weight/(height*height); 
+   String result;
+   System.out.printf("BMI = %.1f",bmi); 			
+   if (bmi <= 20){ 
+      result="Underweight";
+   } 
+   else if (bmi > 20 && bmi < 26) { 
+      result="Normal weight"; 
+   } 
+   else if (bmi >= 26 && bmi < 30) {
+      result="Slightly Overweight"; 
+   } 
+   else if (bmi >= 30 && bmi < 40) {
+      result="Overweight"; 
+   } 
+   else if (bmi >= 40) {
+      result="Extremely Overweight"; 
+   } 
+   else {
+      result="Error"; 
+   } 
+      return result;
+   }
+   public static void main(String[] args) { 
+      float weight=(float)75;
+      float height=(float)1.75;
+      System.out.print("BMI result : "+new BMI().bmi(height,weight)); 
+   } 
+} 
