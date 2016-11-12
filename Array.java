2cfce3f0c9a4d0ec94c28b1153a085d0344fcf93
kimbomin <tbmom13@naver.com>
2016-11-12 22:46:12 +0900

    Array

---
+++ Array.java
@@ -0,0 +1,19 @@
+﻿public class Array {
+   public void sumlist(int[] list) {
+      int sum=0;
+      for (int i=0;i<list.length;i++) {
+         if (list[i]%4==0 && list[i]%5 !=0) {
+            sum=sum+list[i];
+         }
+      }
+      System.out.println(sum);
+   }
+   public static void main(String[] args) {
+      int[] list=new int[1000];
+      for (int i=0;i<1000;i++) {
+         list[i]=i+1;
+      }
+      Array yes=new Array();
+      yes.sumlist(list);
+   }
+}
