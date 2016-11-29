127f1d69b11ad796e43c5331a53e71c38301cef2
kimbomin <tbmom13@naver.com>
2016-11-30 00:41:28 +0900

    Add files via upload

---
+++ LandTurtle.java
@@ -0,0 +1,8 @@
+package com.sd.ch8;
+//import com.sd.ch8.Turtle;
+
+public class LandTurtle extends Turtle {
+    public void walk() {
+        System.out.print("A land turtle walks...");
+    }
+}
---
+++ SeaTurtle.java
@@ -0,0 +1,8 @@
+package com.sd.ch8;
+//import com.sd.ch8.Turtle;
+
+public class SeaTurtle extends Turtle {
+    public void swim() {
+        System.out.print("A sea turtle swims...");
+    }
+}
